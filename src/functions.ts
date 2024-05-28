fcd914e428dc703316c6124cf127b6982d07036e
Mihály Tringel <mtringel@trmsoftwaredesignkft.onmicrosoft.com>
2024-05-28 22:00:56 +0200

    Renaming fullQualifiedMemberNameMatch() to fullQualifiedMemberNameMatchSubTree

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,4 @@
-export const fullQualifiedMemberNameMatch = (path1: string, path2: string) => {
+// either path1 lies below path2 or path2 lies below path1 (startsWith() one way or another)
+export const fullQualifiedMemberNameMatchSubTree = (path1: string, path2: string) => {
   return path1.startsWith(path2) || path2.startsWith(path1);
 };
